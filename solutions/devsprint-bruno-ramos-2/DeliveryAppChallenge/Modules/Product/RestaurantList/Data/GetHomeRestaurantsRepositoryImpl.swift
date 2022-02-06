39c85286e01f10c0a4c133757bc9179b7815c642
Leonardo Mendes <leonardo.mendes@xpi.com.br>
2022-02-06 17:05:29 -0300

    Implement repository with cache

---
+++ solutions/devsprint-bruno-ramos-2/DeliveryAppChallenge/Modules/Product/RestaurantList/Data/GetHomeRestaurantsRepositoryImpl.swift
@@ -0,0 +1,43 @@
+//
+//  GetHomeRestaurantsRepositoryImpl.swift
+//  DeliveryAppChallenge
+//
+//  Created by Leonardo Mendes on 06/02/22.
+//
+
+import Foundation
+
+/// Class used to get data from data source (API/ DB/ UseDefaults/ ETC)
+/// In this class we can implementa a cache strategy too
+final class GetHomeRestaurantsRepositoryImpl {
+    private let primaryDataSource: HomeDataSource
+    private let secondaryDataSource: HomeDataSource
+
+    init(primaryDataSource: HomeDataSource, secondaryDataSource: HomeDataSource) {
+        self.primaryDataSource = primaryDataSource
+        self.secondaryDataSource = secondaryDataSource
+    }
+}
+
+
+extension GetHomeRestaurantsRepositoryImpl: GetHomeRestaurantsRepository {
+    func getHomeRestaurants(completion: @escaping GetHomeRestaurantsCompletion) {
+        primaryDataSource.fetchHomeData { [weak self] result in
+            guard let self = self else {
+                return
+            }
+
+            switch result {
+            case .success(let home):
+                completion(.success(home))
+            case .failure(let error):
+                switch error {
+                case is EmptyCache:
+                    self.secondaryDataSource.fetchHomeData(completion: completion)
+                default:
+                    completion(.failure(error))
+                }
+            }
+        }
+    }
+}
