66d76336845c960763476b41deca50a326ec226a
Alexandre Cardoso <developer.ios.alexandre@gmail.com>
2022-06-14 21:48:43 -0300

    injection and call worker

--- solutions/devsprint-bruno-ramos-4/DeliveryAppChallenge/Modules/Product/RestaurantDetails/Presentation/RestaurantDetailsInteractor.swift
+++ solutions/devsprint-bruno-ramos-4/DeliveryAppChallenge/Modules/Product/RestaurantDetails/Presentation/RestaurantDetailsInteractor.swift
@@ -8,19 +8,34 @@
 import Foundation
 
 protocol RestaurantDetailsBusinessLogic {
-    func loadRestaurantDetails(request: RestaurantDetails.Request)
+    func loadRestaurantDetails()
 }
 
 final class RestaurantDetailsInteractor {
     private let presenter:  RestaurantDetailsPresentationLogic
+    private let worker: RestaurantDetailsWorkerProtocol
 
-    init(presenter:  RestaurantDetailsPresentationLogic) {
+    init(
+        presenter:  RestaurantDetailsPresentationLogic,
+        worker: RestaurantDetailsWorkerProtocol
+    ) {
         self.presenter = presenter
+        self.worker = worker
     }
 }
 
 extension RestaurantDetailsInteractor: RestaurantDetailsBusinessLogic {
-    func loadRestaurantDetails(request: RestaurantDetails.Request) {
-        presenter.presentData(response: .init())
+    
+    func loadRestaurantDetails() {
+        worker.loadRestaurantDetails { [weak self] (result: Result<RestaurantDetail, Error>) in
+            switch result {
+                case let .success(restaurantDetail):
+                    self?.presenter.presentData(response: .success(restaurantDetail))
+                    print(restaurantDetail)
+                case let .failure(error):
+                    self?.presenter.presentData(response: .failure(error))
+                    print(error.localizedDescription)
+            }
+        }
     }
 }
