b4726e33700cdd0962f58819d90b2484d2748668
Tatiana Rico <tatianarico1702@gmail.com>
2022-07-20 17:03:23 -0300

    aplicando SRP nas funções da classe Login

--- solutions/devsprint-pedro-menezes-1/DeliveryAppChallenge/Modules/Product/Scenes/TatianaRico/TRLoginViewController.swift
+++ solutions/devsprint-pedro-menezes-1/DeliveryAppChallenge/Modules/Product/Scenes/TatianaRico/TRLoginViewController.swift
@@ -43,12 +43,16 @@ class TRLoginViewController: UIViewController {
         }
     }
     
+    func alertConection() {
+        let alertController = UIAlertController(title: "Sem conexão", message: "Conecte-se à internet para tentar novamente", preferredStyle: .alert)
+        let actin = UIAlertAction(title: "Ok", style: .default)
+        alertController.addAction(actin)
+        present(alertController, animated: true)
+    }
+    
     @IBAction func loginButton(_ sender: Any) {
         if !ConnectivityManager.shared.isConnected {
-            let alertController = UIAlertController(title: "Sem conexão", message: "Conecte-se à internet para tentar novamente", preferredStyle: .alert)
-            let actin = UIAlertAction(title: "Ok", style: .default)
-            alertController.addAction(actin)
-            present(alertController, animated: true)
+            alertConection()
             return
         }
 
@@ -56,6 +60,10 @@ class TRLoginViewController: UIViewController {
         let parameters: [String: String] = ["email": emailTextField.text!,
                                             "password": passwordTextField.text!]
         let endpoint = Endpoints.Auth.login
+        requestLogin(endpoint: endpoint, parameters: parameters)
+    }
+    
+    func requestLogin(endpoint: String, parameters: [String:String]) {
         AF.request(endpoint, method: .get, parameters: parameters, headers: nil) { result in
             DispatchQueue.main.async {
                 self.stopLoading()
@@ -63,22 +71,30 @@ class TRLoginViewController: UIViewController {
                 case .success(let data):
                     let decoder = JSONDecoder()
                     if let session = try? decoder.decode(Session.self, from: data) {
-                        let vc = UINavigationController(rootViewController: HomeViewController())
-                        let scenes = UIApplication.shared.connectedScenes
-                        let windowScene = scenes.first as? UIWindowScene
-                        let window = windowScene?.windows.first
-                        window?.rootViewController = vc
-                        window?.makeKeyAndVisible()
+                        self.goToHome()
                         UserDefaultsManager.UserInfos.shared.save(session: session, user: nil)
                     } else {
-                        Globals.alertMessage(title: "Ops..", message: "Houve um problema, tente novamente mais tarde.", targetVC: self)
+                        self.alertMensagem(target: self, title: "Ops..", message: "Houve um problema, tente novamente mais tarde.")
                     }
                 case .failure:
                     self.setErrorLogin("E-mail ou senha incorretos")
-                    Globals.alertMessage(title: "Ops..", message: "Houve um problema, tente novamente mais tarde.", targetVC: self)
+                    self.alertMensagem(target: self, title: "Ops..", message: "Houve um problema, tente novamente mais tarde.")
                 }
             }
         }
+    }
+    
+   private func alertMensagem(target: UIViewController, title: String, message: String) {
+        Globals.alertMessage(title: title, message: message, targetVC: target)
+    }
+    
+    private func goToHome() {
+        let vc = UINavigationController(rootViewController: HomeViewController())
+        let scenes = UIApplication.shared.connectedScenes
+        let windowScene = scenes.first as? UIWindowScene
+        let window = windowScene?.windows.first
+        window?.rootViewController = vc
+        window?.makeKeyAndVisible()
     }
     
     @IBAction func showPassword(_ sender: Any) {
@@ -93,14 +109,22 @@ class TRLoginViewController: UIViewController {
     }
     
     @IBAction func resetPasswordButton(_ sender: Any) {
+        self.goToResetPassword()
+    }
+    
+    
+    @IBAction func createAccountButton(_ sender: Any) {
+        self.goToCreatAccount()
+    }
+    
+    func goToResetPassword() {
         let storyboard = UIStoryboard(name: "TRUser", bundle: nil)
         let vc = storyboard.instantiateViewController(withIdentifier: "TRResetPasswordViewController") as! TRResetPasswordViewController
         vc.modalPresentationStyle = .fullScreen
         present(vc, animated: true)
     }
     
-    
-    @IBAction func createAccountButton(_ sender: Any) {
+    func goToCreatAccount() {
         let controller = TRCreateAccountViewController()
         controller.modalPresentationStyle = .fullScreen
         present(controller, animated: true)
@@ -112,25 +136,35 @@ extension TRLoginViewController {
     
     func setupView() {
         heightLabelError.constant = 0
-        loginButton.layer.cornerRadius = loginButton.frame.height / 2
-        loginButton.backgroundColor = .blue
-        loginButton.setTitleColor(.white, for: .normal)
-        loginButton.isEnabled = true
-
         showPasswordButton.tintColor = .lightGray
 
+        setupLoginButton()
+        setupAccountButton()
+        validateButton()
+        emailTextField.setDefaultColor()
+        passwordTextField.setDefaultColor()
+        gestureClickView()
+    }
+    
+    func setupAccountButton() {
         createAccountButton.layer.cornerRadius = createAccountButton.frame.height / 2
         createAccountButton.layer.borderWidth = 1
         createAccountButton.layer.borderColor = UIColor.blue.cgColor
         createAccountButton.setTitleColor(.blue, for: .normal)
         createAccountButton.backgroundColor = .white
-        
-        emailTextField.setDefaultColor()
-        passwordTextField.setDefaultColor()
+    }
+    
+    func setupLoginButton() {
+        loginButton.layer.cornerRadius = loginButton.frame.height / 2
+        loginButton.backgroundColor = .blue
+        loginButton.setTitleColor(.white, for: .normal)
+        loginButton.isEnabled = true
+    }
+    
+    func gestureClickView() {
         let gesture = UITapGestureRecognizer(target: self, action: #selector(didClickView))
         view.addGestureRecognizer(gesture)
         view.isUserInteractionEnabled = true
-        validateButton()
     }
 
     @objc
