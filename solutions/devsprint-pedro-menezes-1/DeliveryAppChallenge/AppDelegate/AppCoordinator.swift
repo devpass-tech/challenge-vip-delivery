494f83a967e797e9a41e67f85e37eacddd0e99e2
carol <carolina.quiterio@ifood.com.br>
2022-07-20 23:57:23 -0300

    Removing my name form App Cordinator

--- solutions/devsprint-pedro-menezes-1/DeliveryAppChallenge/AppDelegate/AppCoordinator.swift
+++ solutions/devsprint-pedro-menezes-1/DeliveryAppChallenge/AppDelegate/AppCoordinator.swift
@@ -4,7 +4,7 @@ struct AppCoordinator {
     static let shared = AppCoordinator()
 
     func getRootViewController() -> UIViewController {
-        let cleanCoder = NewCleanCoders.carol
+        let cleanCoder = NewCleanCoders.thyagoRaphael
 //
         switch cleanCoder {
         case .brunaTokie:
