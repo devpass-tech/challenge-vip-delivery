b9fb8e0fb9a8e48d63f0b3d3b7cd54d6223465fe
Rodrigo Lemos <rodlemos.d@gmail.com>
2022-07-28 17:13:35 -0400

    Apply SRP on login class and names

--- solutions/devsprint-pedro-menezes-1/DeliveryAppChallenge/Modules/Product/Scenes/GabrielCastro/LoginErrors/LoginErrors.swift
+++ solutions/devsprint-pedro-menezes-1/DeliveryAppChallenge/Modules/Product/Scenes/GabrielCastro/LoginErrors/LoginErrors.swift
@@ -10,22 +10,22 @@ import Foundation
 
 struct LoginErrors {
     
-    func setErrorLogin(_ message: String) {
-        errorInLogin = true
-        heightLabelError.constant = 20
-        errorLabel.text = message
-        emailTextField.setErrorColor()
-        passwordTextField.setErrorColor()
-    }
-    
-    func resetErrorLogin(_ textField: UITextField) {
-        heightLabelError.constant = 0
-        if textField == emailTextField {
-            emailTextField.setEditingColor()
-            passwordTextField.setDefaultColor()
-        } else {
-            emailTextField.setDefaultColor()
-            passwordTextField.setDefaultColor()
-        }
-    }
+//    func setErrorLogin(_ message: String) {
+//        errorInLogin = true
+//        heightLabelError.constant = 20
+//        errorLabel.text = message
+//        emailTextField.setErrorColor()
+//        passwordTextField.setErrorColor()
+//    }
+//
+//    func resetErrorLogin(_ textField: UITextField) {
+//        heightLabelError.constant = 0
+//        if textField == emailTextField {
+//            emailTextField.setEditingColor()
+//            passwordTextField.setDefaultColor()
+//        } else {
+//            emailTextField.setDefaultColor()
+//            passwordTextField.setDefaultColor()
+//        }
+//    }
 }
