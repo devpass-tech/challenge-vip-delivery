858c710f33b4f84ec407a80bd2f9ab40903dc178
Brun41v35 <alves.bruno945@gmail.com>
2022-07-28 21:53:29 -0300

    Delete extra argument

--- solutions/devsprint-julio-fernandes-5/DeliveryAppChallenge/Modules/Product/AddressSearch/Presentation/AddressSearchConfigurator.swift
+++ solutions/devsprint-julio-fernandes-5/DeliveryAppChallenge/Modules/Product/AddressSearch/Presentation/AddressSearchConfigurator.swift
@@ -9,9 +9,9 @@ enum AddressSearchConfigurator: FeatureFactory {
     static func make(with dependencies: Dependencies) -> UIViewController {
         let presenter: AddressSearchPresenter = AddressSearchPresenter()
         let interactor: AddressSearchInteractor = AddressSearchInteractor(presenter: presenter)
-        let controller: AddressSearchViewController = AddressSearchViewController(interactor: interactor,
-                                                                                  titleName: dependencies.name)
+        let controller: AddressSearchViewController = AddressSearchViewController(interactor: interactor)
         presenter.controller = controller
+        
         return controller
     }
 }
