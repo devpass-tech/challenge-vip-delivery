131e9980f75f35b33d43b2f30f7df2b16f2c33b0
renato <felicio95.jr@gmail.com>
2023-02-20 18:33:34 -0300

    feat: Adds initial LocalLoaderProtocol and AddressSearch use case to Domain Layer"

---
+++ solutions/devsprint-julio-fernandes-9/DeliveryAppChallenge/Modules/Product/AddressSearch/Domain/UseCase/LocalAddressSearchLoader.swift
@@ -0,0 +1,42 @@
+//
+//  LocalAddressSearchLoader.swift
+//  DeliveryApp
+//
+//  Created by Renato F. dos Santos Jr on 20/02/23.
+//
+
+import Foundation
+import UIKit
+
+protocol LocalLoaderProtocol: AnyObject {
+    typealias LocalLoaderResponse<T: Codable> = ((T) -> Void)
+    func insert<T: Codable>(items: [T])
+    func filter<T: Codable>(by value: String,
+                            completion: @escaping LocalLoaderResponse<T>)
+}
+
+protocol LocalAddressSearchLoaderprotocol: AnyObject {
+    typealias LocalResponse = (([Address]) -> Void)
+    func insert(items: [Address])
+    func filter(by value: String, completion: @escaping LocalResponse)
+}
+
+final class LocalAddressSearchLoader: LocalAddressSearchLoaderprotocol {
+
+    private var localItems: [Address] = []
+
+    func insert(items: [Address]) {
+        // Is there repeated elements?
+        /// NO
+        ///     Get non-repeated items
+        // Append it do self.items
+        guard !items.isEmpty else { return }
+        self.localItems = items
+    }
+
+    func filter(by value: String, completion: @escaping LocalResponse) {
+        let items: [Address] = localItems.filter { $0.contain(value: value) }
+        completion(items)
+    }
+
+}
